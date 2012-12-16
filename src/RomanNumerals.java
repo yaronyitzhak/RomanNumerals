308d2e5f98ff41d8b3343128b6ec0fb5893f385a
Yaron Yitzhak <yaron301343@walla.com>
2012-12-16 08:54:45 +0200

    Success - Test X (10)

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -3,6 +3,36 @@ public class RomanNumerals {
 
 	public String toRoman(int number) {
 		String toRoman = "";
+		
+		
+		if(number>39)
+		{
+			while(number>=50)
+			{
+				toRoman+="L";
+				number-=50;
+			}
+			if(number>=40)
+			{
+				toRoman+="XL";
+				number-=40;
+			}
+		}
+		if(number>8)
+		{
+			while(number>=10)
+			{
+				toRoman+="X";
+				number-=10;
+			}
+			if(number==9)
+			{
+				toRoman+="IX";
+				number-=9;
+			}
+		}
+		
+		
 		if(number>3)
 		{
 			while(number>=5)
@@ -23,6 +53,7 @@ public class RomanNumerals {
 			for(int i=0;i<number;i++)
 				toRoman+= "I";
 		}
+		
 		return toRoman;
 	}
 
