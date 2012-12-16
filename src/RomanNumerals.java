24518c85b7a924a38ca0fdd885f6deed403d73d5
Yaron Yitzhak <yaron301343@walla.com>
2012-12-16 08:47:23 +0200

    Success for the problem with number 3

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,10 +1,29 @@
 
 public class RomanNumerals {
 
-	public String toRoman(int nmber) {
-		if (nmber == 1)
-			return "I";
-		return "II";
+	public String toRoman(int number) {
+		String toRoman="";
+		if(number>3)
+		{
+			while(number>=5)
+			{
+				toRoman+="V";
+				number-=5;
+			}
+			if(number== 4)
+			{
+				toRoman+= "IV";
+				number-=4;
+			}
+
+		}
+		
+		if (number <= 3)
+		{
+			for(int i=0;i<number;i++)
+				toRoman+= "I";
+		}
+		return toRoman;
 	}
 
 }
