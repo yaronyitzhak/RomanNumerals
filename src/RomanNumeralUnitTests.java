6a6445f9ddc14f419eee8ccc11fec6c27a80f5f4
Yaron Yitzhak <yaron301343@walla.com>
2012-12-16 08:58:46 +0200

    Test D (500) - failed

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -11,8 +11,10 @@ public class RomanNumeralUnitTests {
 	public void testThat1ReturnsI() {
 		assertEquals("I", number.toRoman(1));
 		assertEquals("II", number.toRoman(2));
+		assertEquals("II", number.toRoman(3));
 		assertEquals("X", number.toRoman(10));
 		assertEquals("C", number.toRoman(100));
+		assertEquals("D", number.toRoman(500));
 		
 	}
 	
