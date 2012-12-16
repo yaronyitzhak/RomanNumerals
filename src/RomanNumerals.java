51c3aab74de769b952e5a32b0b6bfeed249faa7e
Yaron Yitzhak <yaron301343@walla.com>
2012-12-16 08:57:20 +0200

    Success - Test C (100)

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -5,6 +5,20 @@ public class RomanNumerals {
 		String toRoman = "";
 		
 		
+		if(number>89)
+		{
+			while(number>=100)
+			{
+				toRoman+="C";
+				number-=100;
+			}
+			if(number>=90)
+			{
+				toRoman+="XC";
+				number-=90;
+			}
+		}
+		
 		if(number>39)
 		{
 			while(number>=50)
